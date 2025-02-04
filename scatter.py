11c4f19e42369a83937747a39eec03b53243a9f6
NikshikaChowdary <nikshikasandra@gmail.com>
2025-02-04 08:51:57 +0000

    scatter

---
+++ scatter.py
@@ -0,0 +1,6 @@
+#add scatter plots for the above code
+import matplotlib.pyplot as plt
+plt.scatter(df['WT'],df['MPG'])
+plt.scatter(df['VOL'],df['MPG'])
+plt.scatter(df['SP'],df['MPG'])
+plt.scatter(df['HP'],df['MPG'])
